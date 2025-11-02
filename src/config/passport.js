2b6b1bf3180b77901fffba289557c387dedecb01
DanielSantiagoV <nayzenazq@gmail.com>
2025-11-03 00:05:22 +0100

    feat: :sparkles: Implement JWT authentication strategy with Passport
    This commit introduces a new authentication strategy using Passport and JWT for the FoodStars web platform. Key changes include:
    - Added a Passport configuration file for JWT authentication.
    - Implemented user verification through JWT payload and MongoDB.
    - Enhanced security by utilizing environment variables for secret keys.
    This update strengthens the authentication process and prepares the application for secure user interactions.

---
+++ src/config/passport.js
@@ -0,0 +1,28 @@
+import passport from 'passport';
+import { ExtractJwt, Strategy as JwtStrategy } from 'passport-jwt';
+import dotenv from "dotenv";
+import { obtenerBD } from './db.js';
+import { ObjectId } from 'mongodb';
+
+dotenv.config();
+
+const options = {
+    secretOrKey: process.env.JWT_SECRET_KEY,
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken()
+};
+
+passport.use(
+    new JwtStrategy(options, async (payLoad,done)=>{
+        try {
+            const user = await obtenerBD().collection("usuarios")
+            .findOne({_id: new ObjectId(payLoad.id)});
+
+            if(!user) return done(null, false);
+            return done(null, user);
+        } catch (error) {
+            done(error, false);
+        }
+    })
+)
+
+export default passport;
